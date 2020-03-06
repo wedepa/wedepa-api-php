d769b05124915ec08f2a4cb0a15643e0c7456f61
Wedepa <info@wedepa.com>
2020-03-06 12:43:54 +0100

    Add payment status validation

--- src/Resources/Payment.php
+++ src/Resources/Payment.php
@@ -90,4 +90,117 @@ class Payment extends BaseResource
      * @var array
      */
     public $products;
+
+    /**
+     * Redirect consumer to success page
+     * @return boolean
+     */
+    public function redirectToSuccess()
+    {
+        return
+            $this->isStatusOpen() ||
+            $this->isStatusAuthorized() ||
+            $this->isStatusPaid() ||
+            $this->isStatusPaidPartial() ||
+            $this->isStatusPending();
+    }
+
+    /**
+     * Validate if payment status is open
+     * @return bool
+     */
+    public function isStatusOpen()
+    {
+        return $this->status == 'open';
+    }
+
+    /**
+     * Validate if payment status is failed
+     * @return bool
+     */
+    public function isStatusFailed()
+    {
+        return $this->status == 'failed';
+    }
+
+    /**
+     * Validate if payment status is authorized
+     * @return bool
+     */
+    public function isStatusAuthorized()
+    {
+        return $this->status == 'authorized';
+    }
+
+    /**
+     * Validate if payment status is denied
+     * @return bool
+     */
+    public function isStatusDenied()
+    {
+        return $this->status == 'denied';
+    }
+
+    /**
+     * Validate if payment status is cancel
+     * @return bool
+     */
+    public function isStatusCancel()
+    {
+        return $this->status == 'cancel';
+    }
+
+    /**
+     * Validate if payment status is refund_partial
+     * @return bool
+     */
+    public function isStatusRefundPartial()
+    {
+        return $this->status == 'refund_partial';
+    }
+
+    /**
+     * Validate if payment status is refund
+     * @return bool
+     */
+    public function isStatusRefund()
+    {
+        return $this->status == 'refund';
+    }
+
+    /**
+     * Validate if payment status is paid_partial
+     * @return bool
+     */
+    public function isStatusPaidPartial()
+    {
+        return $this->status == 'paid_partial';
+    }
+
+    /**
+     * Validate if payment status is paid
+     * @return bool
+     */
+    public function isStatusPaid()
+    {
+        return $this->status == 'paid';
+    }
+
+    /**
+     * Validate if payment status is expired
+     * @return bool
+     */
+    public function isStatusExpired()
+    {
+        return $this->status == 'expired';
+    }
+
+    /**
+     * Validate if payment status is pending
+     * @return bool
+     */
+    public function isStatusPending()
+    {
+        return $this->status == 'pending';
+    }
 }
